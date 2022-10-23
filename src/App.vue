b135803a3ef38f834fe79afaa38f2d168e2e4206
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-23 20:53:48 +0200

    Added shift key select multiple boxes function

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,81 @@ export default {
             items: [
                 { text: 'This is an inbox layout.', name:`input-${id}`, id:id++ },
                 { text: 'Check one item', name:`input-${id}`, id:id++ },
-                { text: 'Hold down your Shift key', name:`input-${id}`} ,
+                { text: 'Hold down your Shift key', name:`input-${id}`, id:id++} ,
                 { text: 'Check a lower item', name:`input-${id}`, id:id++ },
                 { text: 'Everything inbetween should also be set to checked', name:`input-${id}`, id:id++ },
                 { text: 'Try to do it without any libraries', name:`input-${id}`, id:id++ },
                 { text: 'Just regular Javascript', name:`input-${id}`, id:id++ },
                 { text: 'Good Luck!', name:`input-${id}`, id:id++ },
                 { text: "Don't forget to tweet your result!", name:`input-${id}`, id:id++ }
-            ] 
+            ],
+            shiftElements: [],
         };
     },
     methods: {
         toggleClass(element) {
             element.classList.toggle('checked');
         },
-        clickHandler(event) {
-            this.toggleClass(event.path[1]);
+        clickHandler(event, id) {
+            const element = event.target.parentNode;
+
+            if(this.shiftElements.length === 2)
+            {
+                this.shiftElements.shift();
+            }
+
+            this.shiftElements.push({
+                id: id,
+                element: element
+            });
+
+            this.toggleClass(element);
+        },
+        shiftClickHandler() {
+            if(this.shiftElements.length === 2)
+            {
+                const state = this.shiftElements[1].element.querySelector('input[type="checkbox"]').checked;
+                const [element1, element2] = this.shiftElements;
+
+                if(element1.id > element2.id)
+                {
+                    let element = element1.element.previousElementSibling;
+                    for(let i = element1.id - 1; i > element2.id; i--)
+                    {
+                        element.querySelector('input[type="checkbox"]').checked = state;
+
+                        if(state && !element.classList.contains('checked'))
+                        {
+                            element.classList.add('checked');
+                        }
+                        else if(!state && element.classList.contains('checked'))
+                        {
+                            element.classList.remove('checked');
+                        }
+
+                        element = element.previousElementSibling;
+                    }
+                }
+                else
+                {
+                    let element = element2.element.previousElementSibling;
+                    for(let i = element2.id - 1; i > element1.id; i--)
+                    {
+                        element.querySelector('input[type="checkbox"]').checked = state;
+
+                        if(state && !element.classList.contains('checked'))
+                        {
+                            element.classList.add('checked');
+                        }
+                        else if(!state && element.classList.contains('checked'))
+                        {
+                            element.classList.remove('checked');
+                        }
+
+                        element = element.previousElementSibling;
+                    }
+                }
+            }
         }
     }
 };
@@ -30,7 +89,7 @@ export default {
 
 <template>
     <div class="item" v-for="item of items" :key="item.id">
-        <input type="checkbox" :id="item.name" @click="clickHandler">
+        <input type="checkbox" :id="item.name" @click="clickHandler($event, item.id)" @click.shift.left="shiftClickHandler()">
         <label :for="item.name">{{item.text}}</label>
     </div>
 </template>
@@ -68,15 +127,12 @@ export default {
         text-decoration: line-through;
     }
 
-    .item:first-child > label {
+    .item:first-child {
         border-top-right-radius: 10px;
     }
 
     .item:last-child {
-        border: none;
-    }
-
-    .item:last-child > label {
+        border-style: none;
         border-bottom-right-radius: 10px;
     }
 </style>
